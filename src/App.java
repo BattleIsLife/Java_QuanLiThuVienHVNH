adc73e1fd699ebbae07082f4ad3b1e1406147a3a
BattleIsLife <nothinginterested123@gmail.com>
2025-04-30 10:35:52 +0700

    Fix lỗi vặt

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
+import model.Nhanvien;
+import model.PermissionLevel;
+
 public class App {
     public String maNhanVien;
     public static void main(String[] args) throws Exception {
-        new GUI_Class.Login_GUI();
+        // new GUI_Class.Login_GUI();
     }
 }
