9752ca0b0fc94394963d8625bfd8a6d10c0bb96f
BattleIsLife <nothinginterested123@gmail.com>
2025-06-05 18:02:06 +0700

    Chuyển hàm main từ login sang 1 file riêng

---
+++ src/App.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		new view.LoginForm();
+	}
+}
